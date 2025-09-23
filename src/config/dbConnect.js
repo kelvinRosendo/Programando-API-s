69df29687ed7267df42b1b1e6d326af510a63697
Kelvin Rosendo <Rosendokelvin8@gmail.com>
2025-09-23 15:42:10 -0300

    Refatora a configuração de conexão com o MongoDB, utilizando variáveis para usuário, senha, host e nome do banco de dados

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,14 @@
-
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://Kelvin:Nivlek_8002delploy-node.spbe0g6.mongodb.net/alura-node");
+// Configuração da conexão com o banco de dados MongoDB usando Mongoose
+
+const usuario = "Kelvin";
+const senha = encodeURIComponent("Nivlek_8002"); // use encodeURIComponent para evitar erro com caracteres especiais
+const host = "delploy-node.spbe.g6.mongodb.net";
+const dbName = "alura-node";
+
+mongoose.connect(`mongodb+srv://Kelvin:Nivlek_8002@delploy-node.spbe.g6.mongodb.net}/"alura-node"`);
 
 let db = mongoose.connection;
 
 export default db;
-
-//Kelvin:Nivlek_8002@
